7463795287d1b6b489b047cb6c6724187f41fafb
Jarrod Finn <jarrod.p.finn@gmail.com>
2021-04-23 09:54:42 -0400

    User data storage

--- app.js
+++ app.js
@@ -1,32 +1,38 @@
-const express = require('express')
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+
 require("dotenv").config();
 //import routes
-const userRoutes = require('./routes/user')
+const userRoutes = require("./routes/user");
 
 //app
 const app = express();
 
 //db connection
-mongoose.connect(
-  process.env.DATABASE,
-  {
+mongoose
+  .connect(process.env.DATABASE, {
     useNewUrlParser: true,
-    useCreateIndex: true
-  }
-)
-.then(() => console.log('DB Connected'))
- 
-mongoose.connection.on('error', err => {
-  console.log(`DB connection error: ${err.message}`)
+    useCreateIndex: true,
+  })
+  .then(() => console.log("DB Connected"));
+
+mongoose.connection.on("error", (err) => {
+  console.log(`DB connection error: ${err.message}`);
 });
- 
+
+// middleware
+app.use(morgan('dev'))
+app.use(bodyParser.json())
+app.use(cookie - parser());
 
 //routes middleware
-app.use("/api", userRoutes); 
+app.use("/api", userRoutes);
 
-const port = process.env.PORT || 8000
+const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`)
+  console.log(`Server is running on port ${port}`);
 });
